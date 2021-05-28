157e8c37fec2a0069e6bf78802d10c2d39f16d6f
Lucas Moura <lucassiyahoo.com.br@gmail.com>
2021-05-28 19:13:03 -0300

    refac: removing unnecessary file

--- Curso JavaScript HCODE/NodeJS_HCODE/routes/klein.js
+++
@@ -1,9 +0,0 @@
-const prefix = '/klein';
-
-module.exports = Route => {
-    Route.get(prefix, (request, response) => {
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/html');
-        response.end('<h1>O Klein eh corno</h1>');
-    });    
-};
