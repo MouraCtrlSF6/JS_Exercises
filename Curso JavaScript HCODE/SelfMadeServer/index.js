1fa8d45b3e672f6405310cf500b74238e63a9ddb
Lucas Moura <lucassiyahoo.com.br@gmail.com>
2021-05-30 13:25:42 -0300

    feat> changing server port

--- Curso JavaScript HCODE/SelfMadeServer/index.js
+++ Curso JavaScript HCODE/SelfMadeServer/index.js
@@ -7,6 +7,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 consign().include('Routes').into(app);
 
-app.listen(3002, '127.0.0.1', () => {
+app.listen(3000, '127.0.0.1', () => {
     console.log('Server is running on port 3000');
 });
