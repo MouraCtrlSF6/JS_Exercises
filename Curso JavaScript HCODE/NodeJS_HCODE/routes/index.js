eb20c2de44eee0714874074ced27ef462ff02688
Lucas Moura <lucassiyahoo.com.br@gmail.com>
2021-09-07 17:07:10 -0300

    feat: removing unnecessary files

--- Curso JavaScript HCODE/NodeJS_HCODE/routes/index.js
+++
@@ -1,9 +0,0 @@
-const HomePageView = require('../views/HomePageView.js');
-
-module.exports = Route => {
-    Route.get('/', (request, response) => {
-        response.statusCode = 200;
-        response.setHeader('Content-Type', 'text/html');
-        response.end(HomePageView); 
-    });
-};
--- Curso JavaScript HCODE/NodeJS_HCODE/utils/error.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    send: (error, request, response, statusCode = 400) => {
-        response    
-            .status(statusCode)   
-            .json({ status: statusCode, message: error });
-    }
-}
